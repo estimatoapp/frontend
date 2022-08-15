e862f16dd800ef0ef6e2e831d111dd325d4687b8
Chandler Roth <hey@chandlerroth.com>
2022-08-15 14:56:29 +0000

    Added tanstack query

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import { useQuery } from '@tanstack/react-query';
 
 function App() {
+  const query = useQuery(['helloWorld'], async () => {
+    const response = await fetch('http://localhost:3001')
+    const data = await response.json()
+    return data
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <p>{query?.data?.hello}</p>
   );
 }
 
