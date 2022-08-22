4151a7d876b6f3ba29b2fa3d62bcf9dc4911c3cf
Chandler Roth <hey@chandlerroth.com>
2022-08-22 10:18:57 -0400

    Install and configure sentry

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { initializeApp } from "firebase/app";
 import { getAnalytics, logEvent } from "firebase/analytics";
 import { config } from './firebase';
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 const app = initializeApp(config);
 const analytics = getAnalytics(app);
 logEvent(analytics, 'testing')
+
+Sentry.init({
+  dsn: "https://7c12400101284d6b9c5cafc707c30697@o1361726.ingest.sentry.io/6677636",
+  integrations: [new BrowserTracing()],
+  tracesSampleRate: 1.0,
+});
 
 const queryClient = new QueryClient()
 
